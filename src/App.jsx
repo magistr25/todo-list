c1e785336d3ff8e5d35ccc5fb0265354e493282b
Ekaterina Miakisheva <magistr25@inbox.ru>
2024-10-15 12:15:00 +0600

    add localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTask } from './features/todo/todoSlice';
 import TaskForm from "./components/TaskForm.jsx";
 import TaskList from "./components/TaskList.jsx";
 import styles from './App.module.css';
 
 export const App = () => {
+    const dispatch = useDispatch();
+    const tasks = useSelector((state) => state.todo.tasks);
+
+    useEffect(() => {
+        const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+        if (savedTasks) {
+            savedTasks.forEach((task) => dispatch(addTask(task)));
+        }
+    }, [dispatch]);
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }, [tasks]);
 
     return (
         <div className={styles.wrapper}>
